d2be480fd9dd399340c65c1165ac1288716ad373
Tyler Delamater <tdelamater@gmail.com>
2023-12-09 23:27:36 -0600

    window by event time total edits by domain

--- src/main/java/xyz/delamater/DataStreamJob.java
+++ src/main/java/xyz/delamater/DataStreamJob.java
@@ -18,61 +18,35 @@
 
 package xyz.delamater;
 
+import com.mongodb.client.model.InsertOneModel;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.connector.mongodb.sink.MongoSink;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
+import org.bson.BsonDocument;
+import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.Duration;
 
 
 public class DataStreamJob {
 
     private static final Logger LOG = LoggerFactory.getLogger(DataStreamJob.class);
 
-    public static void main(String[] args) throws Exception {
-        // Sets up the execution environment, which is the main entry point
-        // to building Flink applications.
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
-
-        String brokers = "192.168.4.130:9092";
-        KafkaSource<EditEvent> source = KafkaSource.<EditEvent>builder().
-                setBootstrapServers(brokers)
-                .setTopics("wikipedia-events")
-                .setGroupId("consumer-group-1")
-                .setStartingOffsets(OffsetsInitializer.latest())
-                .setValueOnlyDeserializer(new JsonNodeDeserializationSchema())
-                .build();
-
-//        String mongoUser = System.getenv("MONGO_USER");
-//        String mongoPass = System.getenv("MONGO_PASS");
-//
-//        MongoSink<String> sink = MongoSink.<String>builder()
-//                .setUri("mongodb://" + mongoUser + ":" + mongoPass + "@192.168.4.100:27017/wikipedia")
-//                .setDatabase("wikipedia")
-//                .setCollection("edits")
-//                .setBatchSize(1000)
-//                .setBatchIntervalMs(1000)
-//                .setMaxRetries(3)
-//                .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
-//                .setSerializationSchema(
-//                        (input, context) -> new InsertOneModel<>(BsonDocument.parse(input)))
-//                .build();
-
-        DataStream<EditEvent> kafkaSource = env
-                .fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
+    public static void runPipeline(StreamExecutionEnvironment env, KafkaSource<EditEvent> source, MongoSink<String> sink) throws Exception {
 
         //{"id":1698671021,
         // "domain":"en.wikipedia.org",
@@ -84,6 +58,13 @@ public class DataStreamJob {
         // "old_length":126239,
         // "new_length":126151}
 
+        DataStream<EditEvent> kafkaSource = env
+                .fromSource(source,
+                        WatermarkStrategy.<EditEvent>forBoundedOutOfOrderness(Duration.ofSeconds(1))
+                                .withWatermarkAlignment("alignment-group-1", Duration.ofSeconds(30), Duration.ofSeconds(10)),
+                        "Kafka Source"
+                );
+
         kafkaSource.filter(new FilterFunction<EditEvent>() {
                     @Override
                     public boolean filter(EditEvent editEvent) throws Exception {
@@ -94,29 +75,71 @@ public class DataStreamJob {
                             return false;
                         }
                     }
-                })
-                .keyBy(editEvent -> editEvent.getDomain())
-                .window(TumblingProcessingTimeWindows.of(Time.seconds(30)))
-                .process(new ProcessWindowFunction<EditEvent, String, String, TimeWindow>() {
+                }).map(new MapFunction<EditEvent, Tuple3<String, Long, Long>>() {
                     @Override
-                    public void process(String s, ProcessWindowFunction<EditEvent, String, String, TimeWindow>.Context context, Iterable<EditEvent> iterable, Collector<String> collector) throws Exception {
-                        long editSizeForWindow = 0L;
-                        for (EditEvent editEvent : iterable) {
-                            long newLength = editEvent.getNew_length();
-                            long oldLength = editEvent.getOld_length();
-                            long diff = newLength - oldLength;
-                            editSizeForWindow += diff;
-                        }
-                        Map<String, Object> result = new HashMap<>();
-                        result.put("domain", s);
-                        result.put("totalEditSize", editSizeForWindow);
-                        collector.collect(new ObjectMapper().writeValueAsString(result));
+                    public Tuple3<String, Long, Long> map(EditEvent editEvent) throws Exception {
+                        return new Tuple3<>(editEvent.getDomain(), editEvent.getNew_length(), editEvent.getOld_length());
                     }
-                }).print();
-
+                })
+                .keyBy(r -> r.f0)
+                .window(TumblingProcessingTimeWindows.of(Time.minutes(5)))
+                .apply(new MyWindowFunction())
+                .sinkTo(sink);
 
         // Execute program, beginning computation.
         env.execute("Flink Java API learning");
+    }
+
+    static class MyWindowFunction implements WindowFunction<Tuple3<String, Long, Long>, String, String, TimeWindow> {
+
+        @Override
+        public void apply(String key, TimeWindow timeWindow, Iterable<Tuple3<String, Long, Long>> iterable, Collector<String> collector) throws Exception {
+            long total = 0L;
+            for (Tuple3<String, Long, Long> tuple : iterable) {
+                total += Math.abs(tuple.f1 - tuple.f2);
+            }
+            Document doc = new Document();
+            doc.put("domain", key);
+            doc.put("edit_size", total);
+            doc.put("start", timeWindow.getStart());
+            doc.put("end", timeWindow.getEnd());
+            collector.collect(doc.toJson());
+        }
+    }
+
+    public static void main(String[] args) {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        String brokers = "192.168.4.130:9092";
+        KafkaSource<EditEvent> source = KafkaSource.<EditEvent>builder().
+                setBootstrapServers(brokers)
+                .setTopics("wikipedia-events")
+                .setGroupId("consumer-group-1")
+                .setStartingOffsets(OffsetsInitializer.latest())
+                .setValueOnlyDeserializer(new JsonNodeDeserializationSchema())
+                .build();
+
+        String mongoUser = System.getenv("MONGO_USER");
+        String mongoPass = System.getenv("MONGO_PASS");
+
+        MongoSink<String> sink = MongoSink.<String>builder()
+                .setUri("mongodb://" + mongoUser + ":" + mongoPass + "@192.168.4.100:27017/wikipedia")
+                .setDatabase("wikipedia")
+                .setCollection("edits")
+                .setBatchSize(1000)
+                .setBatchIntervalMs(1000)
+                .setMaxRetries(3)
+                .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+                .setSerializationSchema(
+                        (input, context) -> new InsertOneModel<>(BsonDocument.parse(input)))
+                .build();
+
+        try {
+            runPipeline(env, source, sink);
+        } catch (Exception e) {
+            LOG.error("Exception in flink pipeline", e);
+            e.printStackTrace();
+        }
     }
 
 }
